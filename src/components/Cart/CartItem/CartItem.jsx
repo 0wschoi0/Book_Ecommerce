50236ee8ec9ca8e37ef4ca41579e8d4f6a281dad
0wschoi0 <83345831+0wschoi0@users.noreply.github.com>
2021-11-25 23:07:34 +0900

    Update CartItem.jsx

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import { Typography, Button, Card, CardActions, CardContent, CardMedia, IconButton } from '@material-ui/core'
 import { Delete, Add, Remove } from '@material-ui/icons'
 
 import useStyles from './styles';
