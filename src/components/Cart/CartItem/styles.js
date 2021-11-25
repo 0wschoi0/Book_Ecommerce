6f77515f4a3d3d6a966ab324b21fe50ff9a8d65f
jinyun0ji <84065432+jinyun0ji@users.noreply.github.com>
2021-11-25 22:59:16 +0900

    Update styles.js

--- src/components/Cart/CartItem/styles.js
+++ src/components/Cart/CartItem/styles.js
@@ -2,17 +2,29 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   media: {
-    height: 260,
+    display: "flex",
+    position: "relative",
+    padding: "150px 0",
+    borderBottom: "1px solid #F5F5F5",
   },
+
   cardContent: {
     display: 'flex',
     justifyContent: 'space-between',
+    marginBottom: "8px",
+    flex: 1,
+    marginLeft: '21px'
   },
   cartActions: {
     justifyContent: 'space-between',
   },
   buttons: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'center'
   },
+  delButton: {
+    border:'none',
+    background: '#47423f',
+    alignItems: 'right'
+  }
 }));
